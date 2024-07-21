08891b9edafb2fd319f4799f4fb7710aa2cda502
azeez-abp <adioadeyoriazeez@gmail.com>
2024-07-21 21:47:48 +0100

    first task corrected

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -9,9 +9,10 @@ def index_range(page: int, page_size: int) -> Tuble[int, int]:
         index_range: function that receive page and
             page_size and return tuple
         Argument:
-            page: int: the page received e.g. page =3
-            page_size: the number of item to be added to the page
+            page(int): the page received e.g. page =3
+            page_size(int): the number of item to be added to the page
         Return:
             Tuple: (int, int)
     """
-    return ((page - 1) * page_size, ((page - 1) * page_size) + page_size)
+    nextPageStartIndex = page * page_size
+    return nextPageStartIndex - page_size, nextPageStartIndex
